03ba9de07e192b4804c20dcea9610da89596d490
teju2312 <87148360+teju2312@users.noreply.github.com>
2024-12-26 20:57:37 +0530

    Add files via upload

---
+++ src/quiz_management_system/Quiz_management_system.java
@@ -0,0 +1,19 @@
+
+package quiz_management_system;
+
+/**
+ *
+ * @author admin
+ */
+public class Quiz_management_system {
+
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new welcome().setVisible(true);
+    }
+    
+}
